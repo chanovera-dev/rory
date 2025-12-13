13bc9dd88e7dccdd60de0bbd988fddf9e5acb93c
Chano Vera <github@chanovera.com>
2025-12-12 19:23:36 -0600

    disable the format post chat/audio

--- functions.php
+++ functions.php
@@ -41,7 +41,7 @@ function setup_rory()
     add_theme_support('automatic-feed-links');
     add_theme_support('custom-logo', array('height' => 32, 'width' => 172, 'flex-height' => true, 'flex-width' => true, ));
     add_theme_support('html5', apply_filters('chanovera_html5_args', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption', 'widgets', 'style', 'script', )));
-    add_theme_support('post-formats', array('aside', 'image', 'video', 'quote', 'link', 'gallery', 'audio', 'chat', ));
+    add_theme_support('post-formats', array('aside', 'image', 'video', 'quote', 'link', 'gallery', ));
     add_theme_support('customize-selective-refresh-widgets');
     add_theme_support('wp-block-styles');
     add_theme_support('align-wide');
